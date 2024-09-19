355a4216148d066c7da9c454fb8619adbe6f9d03
Keuznub <breixopastoriza@gmail.com>
2024-09-19 12:15:38 +0200

    Primera version de calculadora

---
+++ breixobby/index.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll('.button')
+
+for(let i of buttons){
+    let operation = i.getAttribute('data-operation')
+    i.addEventListener('click',() => calculo(operation))
+}
+
+function calculo(operation){
+    let number1 = document.querySelector('.numberEdit1')
+    let number2 = document.querySelector('.numberEdit2')
+    let result = document.querySelector('.result')
+
+    if(!number1.textContent || !number2.textContent) return
+
+    let resultado = eval(number1.textContent+operation+number2.textContent)
+    result.textContent = resultado
+}
+
+
